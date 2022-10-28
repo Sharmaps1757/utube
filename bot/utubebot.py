3cbfbc3697ad916065bbb598480df0a0a17c7074
Sharmaps1757 <105053471+Sharmaps1757@users.noreply.github.com>
2022-10-28 15:57:06 +0530

    Update utubebot.py

--- bot/utubebot.py
+++ bot/utubebot.py
@@ -6,13 +6,13 @@ from .config import Config
 class UtubeBot(Client):
     def __init__(self):
         super().__init__(
-            session_name= "youtubeitbot",
             bot_token= "5226403177:AAEwSR3tC9uo9DrIwxqvWe5RmmWl13earqc",
             api_id= "13593326",
             api_hash= "36366cef731c918dd557ac681e3fe993",
             plugins=dict(root="bot.plugins"),
             workers=6,
-        )
+            )
         self.DOWNLOAD_WORKERS = 6
         self.counter = 0
         self.download_controller = {}
+        
